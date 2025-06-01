99269175de7b743cc027bf6673b79e50d6564f00
Claudio Barrios <cbmne.93@gmail.com>
2025-06-01 13:53:04 -0300

    node ts bases

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import { Server } from "./presentation/server";
+import { envs } from "./config/envs";
+import { AppRoutes } from "./presentation/routes";
+
+(async () => {
+  main();
+})();
+
+function main() {
+  const server = new Server({
+    port: envs.PORT,
+    routes: AppRoutes.routes,
+  });
+
+  server.start();
+}
